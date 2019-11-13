8a22662cddb3a3d932c04ee0854944602ac404cf
Kei Moomin Sugano <tobasojyo@gmail.com>
2019-11-13 20:23:15 +0900

    added custom views

--- app/src/main/java/com/sugano/new_kotlin_app/MainActivity.kt
+++ app/src/main/java/com/sugano/new_kotlin_app/MainActivity.kt
@@ -2,11 +2,15 @@ package com.sugano.new_kotlin_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // setContentView(R.layout.activity_main)
+        val textView  = TextView(this)
+        textView.text = "Hello Custom Views"
+        setContentView(textView)
     }
 }
